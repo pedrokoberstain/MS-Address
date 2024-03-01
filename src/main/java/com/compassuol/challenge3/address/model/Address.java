d158cb218329a45612596e20caeffcadb23f4be7
Kober <pedrokoberstain2@gmail.com>
2024-03-01 15:29:43 -0400

    feat: Adicionando as anotações do JPA

--- src/main/java/com/compassuol/challenge3/address/model/Address.java
+++ src/main/java/com/compassuol/challenge3/address/model/Address.java
@@ -1,7 +1,16 @@
 package com.compassuol.challenge3.address.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Address {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String cep;
