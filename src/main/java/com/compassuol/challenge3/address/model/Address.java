b573a0f3fafa9d1229a0fc59ab6b9824220455dd
Kober <pedrokoberstain2@gmail.com>
2024-03-01 15:29:02 -0400

    feat: Adicionando as variaveis do endereço

--- src/main/java/com/compassuol/challenge3/address/model/Address.java
+++ src/main/java/com/compassuol/challenge3/address/model/Address.java
@@ -2,4 +2,14 @@ package com.compassuol.challenge3.address.model;
 
 public class Address {
 
+    private Long id;
+
+    private String cep;
+    private String street;
+    private String number;
+    private String complement;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private String country;
 }
