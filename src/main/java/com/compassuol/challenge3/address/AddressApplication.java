ff33865421f7f0a529eb11960f1e8347ee85b332
Kober <pedrokoberstain2@gmail.com>
2024-03-01 16:13:20 -0400

    feat: Adicionanddo a anotação no starter da aplicação

--- src/main/java/com/compassuol/challenge3/address/AddressApplication.java
+++ src/main/java/com/compassuol/challenge3/address/AddressApplication.java
@@ -2,8 +2,10 @@ package com.compassuol.challenge3.address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AddressApplication {
 
 	public static void main(String[] args) {
