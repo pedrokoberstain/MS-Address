93e469924976b2776f1e5353e06ff9014d8a61c3
Kober <pedrokoberstain2@gmail.com>
2024-03-01 16:16:12 -0400

    feat: Ajustando o controller e o service para fazer a busca do cep

--- src/main/java/com/compassuol/challenge3/address/controller/AddressController.java
+++ src/main/java/com/compassuol/challenge3/address/controller/AddressController.java
@@ -1,14 +1,27 @@
 package com.compassuol.challenge3.address.controller;
 
+import com.compassuol.challenge3.address.model.Address;
 import com.compassuol.challenge3.address.service.AddressService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/address")
-@RequiredArgsConstructor
 public class AddressController {
 
     private final AddressService addressService;
+
+    @Autowired
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @PostMapping("/verify-and-store")
+    public Address verifyAndStoreAddress(@RequestParam("userId") Long userId, @RequestParam("cep") String cep) {
+        return addressService.verifyAndStoreAddress(userId, cep);
+    }
 }
--- src/main/java/com/compassuol/challenge3/address/service/AddressService.java
+++ src/main/java/com/compassuol/challenge3/address/service/AddressService.java
@@ -1,5 +1,9 @@
 package com.compassuol.challenge3.address.service;
 
+import com.compassuol.challenge3.address.client.ViaCepClient;
+import com.compassuol.challenge3.address.exception.CepNotFoundException;
+import com.compassuol.challenge3.address.model.Address;
+import com.compassuol.challenge3.address.repository.AddressRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +11,23 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AddressService {
 
-    private final AddressService addressService;
+    private final ViaCepClient viaCepClient;
+    private final AddressRepository addressRepository;
+
+    public Address verifyAndStoreAddress(Long userId, String cep) {
+        Address existingAddress = addressRepository.findByCep(cep);
+        if (existingAddress != null) {
+            return existingAddress;
+        }
+
+        Address address = viaCepClient.getAddressByCep(cep);
+
+        if (address == null) {
+            throw new CepNotFoundException("CEP not found: " + cep);
+        }
+
+        address.setUserId(userId);
+
+        return addressRepository.save(address);
+    }
 }
